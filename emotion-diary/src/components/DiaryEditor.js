ad581e47c2e660ecb42ad1d7543e41ffa8f048cf
youngkyu <dudrb7795@gmail.com>
2024-01-11 17:46:39 +0900

    diaryStorage

--- emotion-diary/src/components/DiaryEditor.js
+++ emotion-diary/src/components/DiaryEditor.js
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { useState, useRef, useContext, useEffect } from "react";
-import {DiaryDispatchContext} from "./../App.js"
+import { DiaryDispatchContext } from "./../App.js"
 
 import MyHeader from "./MyHeader";
 import MyButton from "./MyButton";
@@ -9,98 +9,97 @@ import EmotionItem from "./EmotionItem";
 import { getStringDate } from "../util/date.js";
 import { emotionList } from "../util/emotion.js";
 
-
-
-const DiaryEditor = ( {isEdit, originData} ) => {
+const DiaryEditor = ({ isEdit, originData }) => {
 
     const contentRef = useRef();
-    const [ content, setContent] = useState("");
-    const [ emotion, setEmotion] = useState(3);
-    const [ date, setDate ] =useState(getStringDate(new Date));
+    const [content, setContent] = useState("");
+    const [emotion, setEmotion] = useState(3);
+    const [date, setDate] = useState(getStringDate(new Date));
 
-    const {onCreate, onEdit } = useContext(DiaryDispatchContext);
+    const { onCreate, onEdit } = useContext(DiaryDispatchContext);
     const handleClickEmote = (emotion) => {
         setEmotion(emotion);
     }
 
+
     const navigate = useNavigate();
 
     const handleSubmit = () => {
-        if(content.length < 1){
+        if (content.length < 1) {
             contentRef.current.focus();
             return;
         }
 
-        if(window.confirm(isEdit ? "일기를 수정하시겠습니까?" : "새로운 일기를 작성하시겠습니까?")){
-            if(!isEdit){
+        if (window.confirm(isEdit ? "일기를 수정하시겠습니까?" : "새로운 일기를 작성하시겠습니까?")) {
+            if (!isEdit) {
                 onCreate(date, content, emotion);
-            } else{
-                onEdit(originData.id , date, content, emotion);
+            } else {
+                onEdit(originData.id, date, content, emotion);
             }
         }
-      
-        navigate('/', {replace:true})
+
+        navigate('/', { replace: true })
     };
 
     // useEffect : 컴포넌트의 값이 바뀌면 효과를 주는 hooks
-    useEffect( ()=>{
-        if(isEdit){
+    useEffect(() => {
+        if (isEdit) {
             setDate(getStringDate(new Date(parseInt(originData.date))));
             setEmotion(originData.emotion);
             setContent(originData.content);
         }
-    },[isEdit, originData]);
+    }, [isEdit, originData]);
 
     return (
-    <div className="DiaryEditor">
-       <MyHeader 
-        headText={ isEdit ? '일기 수정하기' :'새 일기쓰기'} 
-        leftChild={<MyButton text={'< 뒤로가기'} onClick={ () => navigate(-1) }/>}/>
+        <div className="DiaryEditor">
+            <MyHeader
+                headText={isEdit ? '일기 수정하기' : '새 일기쓰기'}
+                leftChild={<MyButton text={'< 뒤로가기'} onClick={() => navigate(-1)} />} />
 
-        <div>
-            <section>
-                <h4>오늘은 언제인가요?</h4>
-                <div className="input_box">
-                    <input 
-                    className="input_date"
-                    value={date} 
-                    onChange={(e) => setDate(e.target.value)}
-                    type="date" />
-                </div>
-            </section>
+            <div>
+                <section>
+                    <h4>오늘은 언제인가요?</h4>
+                    <div className="input_box">
+                        <input
+                            className="input_date"
+                            value={date}
+                            onChange={(e) => setDate(e.target.value)}
+                            type="date" />
+                    </div>
+                </section>
 
-            <section>
-                <h4>오늘의 감정</h4>
-                <div className="input_box emotion_list_wrapper">
-                    {emotionList.map((it)=>(
-                        <EmotionItem key={it.emotion_id} {...it} 
-                        onClick={handleClickEmote} 
-                        isSelected={it.emotion_id === emotion}
+                <section>
+                    <h4>오늘의 감정</h4>
+                    <div className="input_box emotion_list_wrapper">
+                        {emotionList.map((it) => (
+                            <EmotionItem key={it.emotion_id} {...it}
+                                onClick={handleClickEmote}
+                                isSelected={it.emotion_id === emotion}
+                            />
+                        ))}
+                    </div>
+                </section>
+
+                <section>
+                    <h4>오늘의 일기</h4>
+                    <div className="input_box text_wrapper">
+                        <textarea
+                            placeholder="오늘은 어땠나요?"
+                            ref={contentRef}
+                            value={content}
+                            onChange={(e) => setContent(e.target.value)}
                         />
-                    ))}
-                </div>
-            </section>
+                    </div>
+                </section>
 
-            <section>
-                <h4>오늘의 일기</h4>
-                <div className="input_box text_wrapper">
-                        <textarea 
-                        placeholder="오늘은 어땠나요?"
-                        ref={contentRef} 
-                        value={content} 
-                        onChange={(e) =>setContent(e.target.value)} 
-                        />
-                </div>
-            </section>
-
-            <section>
-                <div className="control_box">
-                    <MyButton text={'취소하기'} onClick={()=> navigate(-1)}/>
-                    <MyButton text={'작성완료'} type={'positive'} onClick={handleSubmit}/>
-                </div>
-            </section>
+                <section>
+                    <div className="control_box">
+                        <MyButton text={'취소하기'} onClick={() => navigate(-1)} />
+                        <MyButton text={'작성완료'} type={'positive'} onClick={handleSubmit} />
+                    </div>
+                </section>
+            </div>
         </div>
-    </div>
     );
 }
 
